a4ee93a565d2749cd7a297f1142bf7e223182964
Chris <stephens-c2@live.com>
2020-04-01 17:06:04 -0700

    creating base files

---
+++ app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Home from "./pages/Home";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container">
+                <home />
+            </div>
+        )
+    }
+}
+export default App;
