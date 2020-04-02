423b7779678ec02bcad5403aba54a6bc9fa2c1ce
Chris <stephens-c2@live.com>
2020-04-01 17:55:22 -0700

    adding components

---
+++ src/components/EmployeeDisplay.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const EmployeeDisplay = (props) => {
+    return (
+        <table className="table">
+            <tr>
+                <th>id</th>
+                <th>Name</th>
+                <th>Role</th>
+                <th>Salary</th>
+            </tr>
+            {props.employee.map(employee => (
+                <tr>
+                    <th scope="row">{employee.id}</th>
+                    <td>{employee.name}</td>
+                    <td>{employee.role}</td>
+                    <td>{employee.salary}</td>
+                </tr>
+            ))}
+        </table>
+    )
+}
+export default EmployeeDisplay
